d819532d35bdceb3d10939c2377fa334b56ed80a
atmcmustafa <mustafa_atmaca57@hotmail.com>
2022-09-08 21:09:40 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+
+
+
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
